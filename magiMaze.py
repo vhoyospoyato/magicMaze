30921cb29ff4790fd2279fe60988b242b7adb3a0
vhoyospoyato <vhoyospoyato@gmail.com>
2018-09-25 03:13:47 +0200

    maze

---
+++ magiMaze.py
@@ -0,0 +1,10 @@
+try:
+
+    number = input('Give a num')
+    number = int(number)
+
+    print("The square number of", number, " is:", number**2 )
+
+except:
+
+    print("Please give me proper number")
