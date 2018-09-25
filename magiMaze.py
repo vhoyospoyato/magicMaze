e146260f36a033455bfad1170526bd9d7b858cfc
vhoyospoyato <vhoyospoyato@gmail.com>
2018-09-25 10:16:07 +0200

    Not correct yet

--- magiMaze.py
+++ magiMaze.py
@@ -1,10 +1,43 @@
-try:
+print("Welcone to the Maze!")
+print("Which way you want to go: N, S, E, W?")
 
-    number = input('Give a num')
-    number = int(number)
+solution = "SSWNES"
 
-    print("The square number of", number, " is:", number**2 )
+lives = 3
 
-except:
+moves = 0
 
-    print("Please give me proper number")
+direction = ""
+
+
+while True:
+
+    temp = input('Enter your move: ')
+
+
+    if temp != "N" and temp != "S" and temp != "E" and temp != "W":
+
+        print("try another way")
+        print("Moves: ", moves)
+        print("Lives: ", lives)
+
+
+        direction = direction+temp
+        moves += 1
+
+
+    elif direction.__contains__(solution):
+
+        print("You made it out!")
+        print("Moves: ", moves)
+        print("Lives: ", lives)
+        break
+
+
+    if moves% 10 == 0:
+
+        lives = lives-1
+
+    if lives == 0:
+
+        print("You got lost in the maze! There is no escape.")
